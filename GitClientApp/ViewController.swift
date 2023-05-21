0ddac6e8c541f5c0bc1bc152d00a42f238cddc99
Guilherme Prates <guilhermeprates@outlook.com>
2023-05-21 14:32:05 -0300

    Start the project

---
+++ GitClientApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GitClientApp
+//
+//  Created by Guilherme Prates on 21/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
